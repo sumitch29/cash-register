85d6635f73fa2ca537067fbf6e73bab811fed51a
Sumit Chavhan <srchavhan29@gmail.com>
2021-10-02 14:55:35 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var btnCalculate = document.querySelector("#btn-calculate");
+var pInfo = document.querySelector("#p-info");
+var tableList = document.querySelectorAll(".no-of-notes");
+var pWords = document.querySelector("#p-words");
+var cashToBeReturned, tempCash;
+var notes = [2000, 500, 100, 20, 10, 5, 1]
 
-const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+btnCalculate.addEventListener("click", onClickCalculate)
 
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
-  hidemessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
-      calculateChange(amountToBeReturned);
-    } else {
-      showMessage("Do You wanna wash plates?");
+function validate(number) {
+    return !isNaN(number) && number.trim().length > 0;
+}
+
+function nonZero(number) {
+    return Number(number) >= 0;
+}
+
+function onClickCalculate() {
+    amount = billAmount.value;
+    given = cashGiven.value;
+    if (validate(amount) && validate(given) && nonZero(amount) && nonZero(given)) {
+        amount = parseInt(amount);
+        given = parseInt(given);
+        if (given >= amount) {
+            if (amount === 0 && given > 0) {
+                pInfo.innerText = "Thanks for the free cash!!!"
+            }
+            else {
+                cashToBeReturned = given - amount;
+                tempCash = cashToBeReturned;
+                pInfo.innerText = "Cash to be returned: ₹" + cashToBeReturned;
+                calculateChange(cashToBeReturned)
+            }
+
+        }
+        else {
+            pInfo.innerText = "We have some openings to wash dishes!";
+        }
     }
-  } else {
-    showMessage("Invalid Bill Amount");
-  }
-});
-
-function calculateChange(amountToBeReturned) {
-  for (let i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
-
-    noOfNotes[i].value = numberOfNotes;
-  }
+    else {
+        pInfo.innerText = "INVALID"
+    }
 }
 
-function hidemessage() {
-  message.style.display = "none";
-}
+function calculateChange(cashToBeReturned) {
+    var words = ""
+    for (var i = 0; i < notes.length; i++) {
+        var noOfNotes = Math.trunc(cashToBeReturned / notes[i])
+        tableList[i].innerText = noOfNotes;
+        cashToBeReturned = cashToBeReturned % notes[i];
 
-function showMessage(msg) {
-  message.style.display = "block";
-  message.innerText = msg;
+        // for words
+        if (noOfNotes > 0) {
+            words += noOfNotes + " x ₹" + notes[i] + " = " + noOfNotes * notes[i] + "\n"
+        }
+    }
+    words += "--------\nReturn: ₹" + tempCash;
+    pWords.innerText = words;
 }
